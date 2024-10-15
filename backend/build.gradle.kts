3379271eed5b9a559ac5c5e58548a6f7cb82fe31
Manuel Mangalathu <manuelmangalathu@gmail.com>
2024-10-15 22:50:35 +0200

    add postgres jdbc driver dependency

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -13,6 +13,9 @@ dependencies {
     implementation("org.mockito:mockito-core:5.13.0")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    // https://mvnrepository.com/artifact/org.postgresql/postgresql
+    implementation("org.postgresql", "postgresql", "42.7.4")
 }
 
 tasks.test {
