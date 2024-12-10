7fe24f82e7f88b228ef67f210c39a0093badcdba
Manuel Mangalathu <manuelmangalathu@gmail.com>
2024-12-10 22:56:30 +0100

    execute prepare statement to insert dummy values

--- backend/src/main/java/org/maney/data/DatabaseInitializer.java
+++ backend/src/main/java/org/maney/data/DatabaseInitializer.java
@@ -18,27 +18,47 @@ public class DatabaseInitializer {
 
             Connection db = DriverManager.getConnection(url, props);
 
+            _insertDummyValues(db);
+
             Statement st = db.createStatement();
-            ResultSet rs = st.executeQuery("SELECT * FROM income");
+            ResultSet rs = st.executeQuery("SELECT name FROM income_categories");
 
             while (rs.next()) {
-                System.out.println(rs.getString(1));
+                System.out.println(rs.getArray(1));
             }
 
             rs.close();
             st.close();
 
+            db.close();
+
         } catch (Exception e) {
-            if(e instanceof IOException) {
+            if (e instanceof IOException) {
                 System.err.println("Failed to load database config file " + configFilePath);
             }
 
-            if(e instanceof SQLException) {
-                System.err.println("Failed to connect to database");
+            if (e instanceof SQLException) {
                 System.err.println(e.getMessage());
             }
-
-            System.err.println(e.getMessage());
         }
+    }
+
+    private static void _insertDummyValues(Connection db) throws SQLException {
+        String insertCategorySqlString = "INSERT INTO income_categories (\n" +
+                "    income_category_id,\n" +
+                "    name\n" +
+                ") VALUES( ?, ?)";
+        PreparedStatement insertCategory = db.prepareStatement(insertCategorySqlString);
+
+        insertCategory.setString(1, "71c96b9d-f788-4941-923a-aeb6b0a6bee2");
+        insertCategory.setString(2, "category A");
+
+        ResultSet rs = insertCategory.executeQuery();
+        while (rs.next()) {
+            System.out.println(rs);
+        }
+
+        rs.close();
+        insertCategory.close();
     }
 }
